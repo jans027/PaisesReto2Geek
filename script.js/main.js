4144bb4cd691da3f03e106a5bc487bb0cd601401
jans027 <87904137+jans027@users.noreply.github.com>
2022-06-12 15:04:44 -0500

    Update main.js

--- script.js/main.js
+++ script.js/main.js
@@ -48,7 +48,7 @@ function mostrarDestinos(e) {
 
 
     switch (button) {
-        case 'https://jans027.github.io/PaisesReto2Geek/images/AFRICA.png': 
+        case 'https://jans027.github.io/PaisesReto2Geek/images/AFRICA.png': {
             const tarjetasDestinos = paises.filter(pais => pais.id == 1); //obtengo el objeto por el  id
 
             const template = document.querySelector('#template2').content;
@@ -68,8 +68,9 @@ function mostrarDestinos(e) {
 
             });
             tarjeta.appendChild(fragment)
+        }
             break;
-        case 'https://jans027.github.io/PaisesReto2Geek/images/ASIA.png': 
+        case 'https://jans027.github.io/PaisesReto2Geek/images/ASIA.png': {
 
             const tarjetasDestinos = paises.filter(pais => pais.id == 2); //obtengo el objeto por el  id
 
@@ -90,6 +91,7 @@ function mostrarDestinos(e) {
 
             });
             tarjeta.appendChild(fragment)
+        }
             break;
         case 'https://jans027.github.io/PaisesReto2Geek/images/EUROPA.png': {
             const tarjetasDestinos = paises.filter(pais => pais.id == 3); //obtengo el objeto por el  id
@@ -111,8 +113,9 @@ function mostrarDestinos(e) {
 
             });
             tarjeta.appendChild(fragment)
+        }
             break; 
-        case'https://jans027.github.io/PaisesReto2Geek/images/OCEANIA.png': 
+        case'https://jans027.github.io/PaisesReto2Geek/images/OCEANIA.png': {
             const tarjetasDestinos = paises.filter(pais => pais.id == 4); //obtengo el objeto por el  id
 
             const template = document.querySelector('#template2').content;
@@ -132,8 +135,9 @@ function mostrarDestinos(e) {
 
             });
             tarjeta.appendChild(fragment)
+        }
             break;
-        case 'https://jans027.github.io/PaisesReto2Geek/images/AMERICA.png': 
+        case 'https://jans027.github.io/PaisesReto2Geek/images/AMERICA.png': {
             const tarjetasDestinos = paises.filter(pais => pais.id == 5); //obtengo el objeto por el  id
 
             const template = document.querySelector('#template2').content;
@@ -153,6 +157,7 @@ function mostrarDestinos(e) {
 
             });
             tarjeta.appendChild(fragment)
+        }
             break; 
         default:
             alert('el boton no existe ' + button)
